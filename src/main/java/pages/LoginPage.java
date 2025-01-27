dd830ea0756a527afbfbad62ba1480d84af8ba38
catfishjim <jjnospam68@gmail.com>
2025-01-27 08:13:05 -0500

    third

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,7 +5,7 @@ public class LoginPage {
 	//String loginText =  "//*[@id='1-email']";
 	public static String loginText =  "//div[@title='Welcome Back!']";
 	//String emailAddress = "//input[@placeholder='Email']";
-	//String passwordfield = "//input[@placeholder='Password']";a
+	//String passwordfield = "//input[@placeholder='Password']";
 	//ignore
 	public static String emailAddress = "//*[@id='1-emaill']";
 	public static String passwordfield = "//*[@id='password-content']";
